66bdc335029c8fe932fcef2614ea4f93b07b46e0
Dwiki Krisna Saputra <dwiki1968@gmail.com>
2022-05-28 13:26:41 +0700

    problem solved

--- index.js
+++ index.js
@@ -1,20 +1,103 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const firstUser = {};
-const secondUser = {};
+
+const itemUnik = (array) => {
+  let setItem = new Set(array);
+  let arrayItem = [...setItem];
+  //   console.log("tes", arrayItem);
+  return arrayItem;
+};
+
+const firstUser = {
+  name: "Monica",
+  gender: "Female",
+  age: 17,
+  email: "monica@dingdong.com",
+  favoriteColor: itemUnik(["Yellow", "Pink", "White", "Purple"]),
+  isHavePet: true,
+  education: [
+    {
+      name: "SD 01",
+      city: "Jakarta",
+      graduate: "2016",
+    },
+    {
+      name: "SMP 02",
+      city: "Jakarta",
+      graduate: "2019",
+    },
+    {
+      name: "SMA 03",
+      city: "Jakarta",
+    },
+  ],
+  favoriteRestaurant: itemUnik([
+    "Bento",
+    "Sushi",
+    "Pancake",
+    "Eggy",
+    "Tempura",
+    "Bento",
+    "Eggy",
+    "Padang",
+    "Tteok",
+    "Sushi",
+    "Sushi",
+  ]),
+};
+const secondUser = {
+  name: "Wendy",
+  gender: "Male",
+  age: 23,
+  email: "wendy@dingdong.com",
+  favoriteColor: itemUnik(["Blue", "Black", "Grey"]),
+  isHavePet: false,
+  education: [
+    {
+      name: "SD 02",
+      city: "Jakarta",
+      graduate: "2010",
+    },
+    {
+      name: "SMP 03",
+      city: "Bogor",
+      graduate: "2013",
+    },
+    {
+      name: "SMA 01",
+      city: "Surabaya",
+      graduate: "2016",
+    },
+    {
+      name: "Universitas Maju",
+      city: "Tangerang",
+    },
+  ],
+  favoriteRestaurant: itemUnik([
+    "Tempura",
+    "Bento",
+    "Sushi",
+    "Pancake",
+    "Padang",
+    "Katsu",
+    "Geprek",
+    "Pancake",
+    "Eggy",
+  ]),
+};
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
-const users = [];
+const users = [firstUser, secondUser];
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
-    console.log(users.length || users.size);
-    console.log(users);
+  console.log(users.length || users.size);
+  console.log(users);
 }
 
 main();
 
 module.exports = {
-    users
+  users,
 };
